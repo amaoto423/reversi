d81399b760c0100aea3fa97252f1d91bf30c1cfb
amane <hashimoto-naoki-93@kayac.com>
2024-07-28 15:38:55 +0900

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    let revData=new RevData();
+    const gameCanvas=GameCanvas.addBgCanvas('#reversi',1200,800);
+    console.log(gameCanvas);
+})
