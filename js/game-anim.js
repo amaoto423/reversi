4108bca8b0fc18142a789a451727ecfd1c64320e
amane <amaoto423@gmail.com>
2024-08-07 22:49:17 +0900

    アニメーションを学んだ

---
+++ js/game-anim.js
@@ -0,0 +1,25 @@
+const gameAnim = { holder: [], time: { old: 0, now: 0, diff: 0, sum: 0 } };
+
+gameAnim.start = function () {
+  this.time.old = Date.now();
+  const func = () => {
+    this.update();
+    requestAnimationFrame(func);
+  };
+  func();
+};
+
+gameAnim.update = function () {
+  this.time.now = Date.now();
+  this.time.diff = this.time.now - this.time.old;
+  this.time.sum += this.time.diff;
+  this.time.old = this.time.now;
+  this.holder.forEach((x) => x.func());
+};
+
+gameAnim.add = function (id, func) {
+  this.holder.push({ id, func });
+};
+gameAnim.remove = function (id) {
+  this.holder.filter((x) => x.id !== id);
+};
--- js/main.js
+++ js/main.js
@@ -7,4 +7,17 @@ document.addEventListener("DOMContentLoaded", async function () {
   console.log(r);
   appView.init();
   appView.update();
+
+  gameAnim.add("main", () => {
+    appView.update();
+  });
+
+  gameAnim.start();
+  console.log(revData.blank);
+  await gameUtil.sleep(1000);
+  revCore.data.board[3][3] = RevData.blank;
+
+  await gameUtil.sleep(1000);
+
+  revCore.data.board[4][4] = RevData.blank;
 });
